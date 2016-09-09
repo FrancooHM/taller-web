0a0bb0862b8fa696198ed0ba359d716def27a977
Franco Rabaglia <francoo.hd@gmail.com>
2016-09-09 20:08:08 -0300

    hola aula

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,15 @@ router.get('/example', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.send({
-    // data: 'fetch("http://138.68.3.211:3000/example", { method: "get" }).then(function(response) { return response.json(); }).then(function(responseJson) { console.debug("responseJson: " + responseJson.data); let element = document.getElementById("content"); element.innerHTML = responseJson.data; });'
     data: "Hola Aula Magna!"
+  });
+});
+
+router.get('/code', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.send({
+    data: 'fetch("http://138.68.3.211:3000/example", { method: "get" }).then(function(response) { return response.json(); }).then(function(responseJson) { console.debug("responseJson: " + responseJson.data); let element = document.getElementById("content"); element.innerHTML = responseJson.data; });'
   });
 });
 
