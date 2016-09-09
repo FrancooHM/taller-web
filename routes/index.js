4244909e4f080735e198f6afa1d981a2933dc492
Franco Rabaglia <francoo.hd@gmail.com>
2016-09-09 02:45:48 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 router.get('/example', function(req, res, next) {
-  res.send({data: "Esto es un dato de un servidor."});
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.send({
+    data: "Esto es un dato de un servidor."
+  });
 });
 
 // router.get('/', function(req, res, next) {
